f6c8c760dcdf14cdc6364cfc4a5d9b2ce89fadc7
Aare Palm <aarepalm@gmail.com>
2018-11-12 10:49:15 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+struct Interface
+{
+    Interface() {std::cout << "Interface()" << std::endl;}
+    virtual ~Interface() {std::cout << "~Interface()" << std::endl;}
+ 
+    virtual int SomeGetter()=0;
+};
+
+struct Impl : public Interface
+{
+    Impl() {std::cout << "Impl()" << std::endl;}
+    virtual ~Impl() {std::cout << "~Impl()" << std::endl;}
+    
+    virtual int SomeGetter() {return 1;}
+};
+
+
+int main()
+{
+    Impl impl;
+}
